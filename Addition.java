f9e27545d644642b1de55fcd5925202a064c87bc
GK01-DEVALOPER <devopsc478@gmail.com>
2026-06-10 11:49:41 +0530

    Update Addition.java

--- Addition.java
+++ Addition.java
@@ -2,9 +2,7 @@ public class Addition{
 	public static void main (String[]args){
 		int num1 = 10;
 		int num2 = 5;
-		int sum =num1 + num2;
 		int prod =num1 * num2;
-		System.out.println("the sum is:" +sum);
 		System.out.println("the product is:" *prod);
 	}
 }
