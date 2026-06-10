70033ceafe4fe94e171ca98cf46ceb0b630c5be0
GK01-DEVALOPER <devopsc478@gmail.com>
2026-06-10 10:34:27 +0530

    Update Addition.java
    git commint -m "updated "

--- Addition.java
+++ Addition.java
@@ -1,7 +1,7 @@
 public class Addition{
 	public static void main (String[]args){
 		int num1 = 10;
-		int num2 = ;
+		int num2 = 5;
 		int sum =num1 + num2;
 		System.out.println("the sum is:" +sum);
 	}
